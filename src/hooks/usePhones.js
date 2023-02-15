b884d5f768bae0917113df8c7992be7d74378450
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-15 22:14:12 +0100

    Add cache to custom hook

--- src/hooks/usePhones.js
+++ src/hooks/usePhones.js
@@ -1,17 +1,26 @@
 import axios from "axios";
+import { useCallback } from "react";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const usePhones = () => {
-  const getPhones = async () => {
-    try {
-      const response = await axios.get(`${apiUrl}`);
+  const getPhones = useCallback(async () => {
+    const localdata = localStorage.getItem("phones");
+    const cache = JSON.parse(localdata);
 
-      return response.data;
-    } catch (error) {
-      throw error;
+    if (localdata && !(cache.expire <= Date.now())) {
+      return cache.data;
     }
-  };
+
+    const response = await axios.get(`${apiUrl}`);
+
+    localStorage.setItem(
+      "phones",
+      JSON.stringify({ data: response.data, expire: Date.now() + 3600000 })
+    );
+
+    return response.data;
+  }, []);
 
   return { getPhones };
 };
