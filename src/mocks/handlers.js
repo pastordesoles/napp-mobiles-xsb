ef85422f899e51b400968423a17afde40008b698
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-16 22:04:11 +0100

    Set up msw handlers

---
+++ src/mocks/handlers.js
@@ -0,0 +1,12 @@
+import { rest } from "msw";
+import phoneMock from "./phoneMock";
+
+let apiUrl = process.env.REACT_APP_API_URL;
+
+const handlers = [
+  rest.get(`${apiUrl}`, (request, response, context) => {
+    return response(context.status(200), context.json(phoneMock));
+  }),
+];
+
+export default handlers;
