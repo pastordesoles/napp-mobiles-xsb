a536de645071ace0a60051d628a269d4106bf936
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-16 18:34:22 +0100

    Format list and phone card

--- src/components/PhoneCardList/PhoneCardList.jsx
+++ src/components/PhoneCardList/PhoneCardList.jsx
@@ -7,8 +7,20 @@ const PhoneCardList = ({ phones }) => {
         {phones.map((phone) => (
           <li key={phone.id} className="phone">
             <article>
-              <h3>{`${phone.brand} ${phone.model}`}</h3>
-              <img src={phone.imgUrl} alt={phone.model} />
+              <h3 className="phone__name">{`${phone.brand} - ${phone.model}`}</h3>
+              <img
+                className="phone__image"
+                width="100"
+                height="100"
+                loading="lazy"
+                src={phone.imgUrl}
+                alt={phone.model}
+              />
+              {phone.price ? (
+                <span className="phone__price">{`${phone.price}€`}</span>
+              ) : (
+                <span className="phone__price">{`Ask us how much!`}</span>
+              )}
             </article>
           </li>
         ))}
--- src/components/PhoneCardList/PhoneCardListStyled.jsx
+++ src/components/PhoneCardList/PhoneCardListStyled.jsx
@@ -4,7 +4,7 @@ const PhoneCardListStyled = styled.ul`
   display: grid;
   row-gap: 30px;
   grid-template-columns: repeat(1, 1fr);
-  padding: 2rem 0;
+  padding: 2rem;
   justify-items: center;
   justify-content: center;
 
@@ -18,8 +18,47 @@ const PhoneCardListStyled = styled.ul`
   @media only screen and (min-width: 1150px) {
     align-items: unset;
 
-    grid-template-columns: repeat(3, 240px);
-    column-gap: calc((100% - (3 * 240px)) / 3);
+    grid-template-columns: repeat(4, 240px);
+    column-gap: calc((100% - (4 * 240px)) / 3);
+  }
+
+  article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+    border: 1px solid black;
+    height: 250px;
+    width: 250px;
+    background-color: white;
+    padding: 0.5rem;
+    border-radius: 15px;
+
+    img {
+      object-fit: contain;
+    }
+  }
+
+  .phone {
+    &__name {
+      font-size: 1.125rem;
+      text-align: center;
+    }
+
+    &__price {
+      color: ${(props) => props.theme.colors.mainColors.dark};
+      font-size: 1.125rem;
+      font-weight: ${(props) => props.theme.fontWeights.bold};
+    }
+  }
+
+  @media only screen and (min-width: 1150px) {
+    article {
+      :hover {
+        background-color: #bebebe;
+      }
+    }
   }
 `;
 
