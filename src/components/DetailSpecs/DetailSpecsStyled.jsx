90ac3666943108b68eac22b121aec4a526541cf9
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-17 20:21:32 +0100

    Modify detailSpecs style

--- src/components/DetailSpecs/DetailSpecsStyled.jsx
+++ src/components/DetailSpecs/DetailSpecsStyled.jsx
@@ -7,11 +7,6 @@ const DetailSpecsStyled = styled.ul`
   flex-direction: column;
   box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
   transition: all 0.3s;
-
-  &:hover {
-    box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2),
-      0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  }
 `;
 
 export default DetailSpecsStyled;
