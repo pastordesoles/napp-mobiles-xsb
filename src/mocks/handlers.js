d3fa04b935b170c573d97beaffb82431c92d2be6
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-18 12:44:11 +0100

    Remove unused handler

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,15 +2,10 @@ import { rest } from "msw";
 import { phoneMock } from "./phoneMock";
 
 let apiUrl = process.env.REACT_APP_API_URL;
-const apiCart = process.env.REACT_APP_API_URL_CART;
 
 const handlers = [
   rest.get(`${apiUrl}`, (request, response, context) => {
     return response(context.status(200), context.json(phoneMock));
-  }),
-
-  rest.post(`${apiCart}`, (request, response, context) => {
-    return response(context.status(200), context.json({ count: 1 }));
   }),
 ];
 
