41f94a4e3109a42c40ccb68d2b3a33a09bba86e0
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-18 12:40:55 +0100

    Add details page test

--- src/pages/DetailsPage/DetailsPage.test.jsx
+++ src/pages/DetailsPage/DetailsPage.test.jsx
@@ -1,13 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import DetailsPage from "./DetailsPage";
 import mainTheme from "../../styles/mainTheme";
 import GlobalStyle from "../../styles/GlobalStyles";
+import usePhones from "../../hooks/usePhones";
+
+jest.mock("../../hooks/usePhones");
 
 describe("Given a ProductListPage component", () => {
+  let mockGetPhoneDetail;
+
+  beforeEach(() => {
+    mockGetPhoneDetail = jest.fn();
+    usePhones.mockReturnValue({
+      getPhoneDetail: mockGetPhoneDetail,
+    });
+  });
+
   describe("When it's rendered", () => {
-    test("Then it should show a input search", () => {
+    test("Then it should show a phone image", () => {
       render(
         <BrowserRouter>
           <ThemeProvider theme={mainTheme}>
@@ -19,7 +31,28 @@ describe("Given a ProductListPage component", () => {
 
       const image = screen.queryByRole("img");
 
+      expect(mockGetPhoneDetail).toHaveBeenCalled();
       expect(image).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered and phoneDetail is undefined", () => {
+    test("Then it should show 'Ups...We are out of signal'", async () => {
+      mockGetPhoneDetail.mockReturnValue(undefined);
+
+      render(
+        <BrowserRouter>
+          <ThemeProvider theme={mainTheme}>
+            <GlobalStyle />
+            <DetailsPage />
+          </ThemeProvider>
+        </BrowserRouter>
+      );
+
+      await waitFor(() => {
+        const noResults = screen.queryByText("Ups...We are out of signal");
+        expect(noResults).toBeInTheDocument();
+      });
     });
   });
 });
